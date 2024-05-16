9577f3989530807f8a6700ea099cab21c06e92ff
MaksimWBC <maksimkorneenko@gmail.com>
2024-05-16 17:57:54 +0300

    last commit

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const getNum = document.getElementById("btn")
 const randomNum = document.getElementById("num")
 const numMin = document.getElementById("number1")
 const numMax = document.getElementById("number2")
+const chekBtn1 = document.getElementById("check1")
+const chekBtn2 = document.getElementById("check2")
+const textArray = document.getElementById("text_array")
+const parameters = document.getElementById("container")
 
 
-getNum.addEventListener('click', () => {
+parameters.addEventListener('change', () => {
+    if(chekBtn1.checked) {
+        textArray.style.opacity = '0';
+    } else {
+        textArray.style.opacity = '1';
+    }
+});
 
-    randomNum.innerHTML = Math.floor(Math.random() * (Number(numMax.value)- Number(numMin.value) + 1) + Number(numMin.value))
+textArray.addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/[^\s\d]/g, '');
+})
+
+getNum.addEventListener('click', (e) => {
+    if(chekBtn1.checked) {
+        randomNum.innerHTML = Math.floor(Math.random() * (Number(numMax.value)- Number(numMin.value) + 1) + Number(numMin.value))
+    }
+    if(chekBtn2.checked) {
+        let arrayNum = textArray.value.trim().split(/\s+/);
+        randomNum.innerHTML = arrayNum[Math.floor(Math.random()*arrayNum.length)];
+    }
 })
